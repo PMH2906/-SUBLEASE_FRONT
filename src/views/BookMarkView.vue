b7530b95736abe6b6cba8786e5589ee8abbd372d
YangHyunYong <9935ok9938@gmail.com>
2022-11-21 17:20:32 +0900

    temp commit

---
+++ src/views/BookMarkView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav>
+        관심 매물 
+        |
+         관심 지역
+    </nav>
+    <div>
+        
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
